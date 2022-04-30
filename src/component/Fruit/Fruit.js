1c1b8d5c0759723e467f660b0fbcb31a71ab27c8
neloy005 <arifistiekofficial@gmail.com>
2022-04-30 17:43:07 +0600

    data successfully loaded from backend

---
+++ src/component/Fruit/Fruit.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Fruit = (props) => {
+    console.log(props.fruit)
+    const { name, image } = props.fruit;
+    return (
+        <div>
+            <h2>Single fruit</h2>
+            <img src={image} alt="" />
+        </div>
+    );
+};
+
+export default Fruit;
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import banner from '../../images/fruits1.jpg';
+import Fruit from '../Fruit/Fruit';
 import './Home.css'
 
 const Home = () => {
+    const [fruits, setFruits] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/fruit')
+            .then(res => res.json())
+            .then(data => setFruits(data))
+    }, [])
     return (
-        <div className='banner-container'>
-            <img className='banner' src={banner} alt="" />
-            <p className='tagline' ><span style={{ 'color': '' }}>Our fruit</span>  Inventory</p>
-            <p className='tagline-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Quam et eius blanditiis cumque doloribus nostrum.</p>
+        <div>
+            <div className='banner-container'>
+                <img className='banner' src={banner} alt="" />
+                <p className='tagline' ><span style={{ 'color': '' }}>Our fruit</span>  Inventory</p>
+                <p className='tagline-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Quam et eius blanditiis cumque doloribus nostrum.</p>
+            </div>
+
+
+            <h2>Inventory Overview:</h2>
+            {
+                fruits.map(fruit => <Fruit
+                    key={fruit._id}
+                    fruit={fruit}
+                ></Fruit>)
+            }
+
 
         </div>
     );
