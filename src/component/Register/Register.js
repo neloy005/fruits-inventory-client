e06836f8aea91b0ca4d86429bc55c81e20ef0f46
neloy005 <arifistiekofficial@gmail.com>
2022-05-01 21:36:25 +0600

    registration process added and spinner added

--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Spinner } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Register = () => {
+    const [errorMessage, setErrorMessage] = useState('');
 
     const navigate = useNavigate();
     const navigateToLogin = () => {
         navigate('/login');
+    }
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+    const createNewUser = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        if (password.length < 6) {
+            setErrorMessage('Password must be 6 charachters or more!');
+        }
+        else {
+            setErrorMessage('');
+        }
+        createUserWithEmailAndPassword(email, password);
+    }
+
+    if (user) {
+        navigate('/');
+    }
+    if (loading) {
+        return <Spinner animation="border" variant="success" />
     }
 
 
@@ -16,8 +49,9 @@ const Register = () => {
 
             <form onSubmit={createNewUser}>
                 <input type="text" name='name' placeholder='Enter your name' required /> <br />
-                <input type="text" name='email' placeholder='Enter email' required /> <br />
+                <input type="email" name='email' placeholder='Enter email' required /> <br />
                 <input type="password" name='password' placeholder='Enter password' required /> <br />
+                <p style={{ 'color': 'red' }}>{errorMessage} </p>
                 <input className='submit-btn' type="submit" value='Register' />
             </form>
             <br />
