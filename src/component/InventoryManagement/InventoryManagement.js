cc74bd519bfc832c2a901d2ec9897f4e2babf113
neloy005 <arifistiekofficial@gmail.com>
2022-05-03 04:53:56 +0600

    restock portion done with error handling

--- src/component/InventoryManagement/InventoryManagement.js
+++ src/component/InventoryManagement/InventoryManagement.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const InventoryManagement = () => {
     const { id } = useParams();
-
     const [fruit, setFruit] = useState({});
-    const [fruitAfterDecrease, setFruitAfterDecrease] = useState(0);
     const [fruitCount, setFruitCount] = useState(0)
+    const [error, setError] = useState('')
 
     useEffect(() => {
         const url = `http://localhost:5000/fruit/${id}`;
@@ -38,6 +39,45 @@ const InventoryManagement = () => {
             })
     }
 
+    const handleRestock = event => {
+        event.preventDefault();
+        const restockValue = event.target.restock.value;
+        const restock = parseInt(restockValue);
+        if (restock <= 0 || (restockValue === '')) {
+            setError('Error: Stock amount must be minimum 1');
+            return;
+        }
+        else {
+            setError('');
+            quantity = quantity + restock;
+            setFruitCount(quantity);
+            const decreasedQuantity = { quantity: quantity };
+            const url = `http://localhost:5000/fruit/${id}`;
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(decreasedQuantity)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    // console.log('success', data);
+
+                    toast.success(`restocked ${restock} items!`, {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                    });
+                    event.target.reset();
+                })
+        }
+
+    }
 
 
     return (
@@ -46,6 +86,22 @@ const InventoryManagement = () => {
             <p>{fruit.name}</p>
             <p>{fruitCount}</p>
             <button onClick={handleDecrease}>Delivered</button>
+            <form onSubmit={handleRestock}>
+                <input type="number" name='restock' placeholder='Enter restock amount' /> <br />
+                <p style={{ 'color': 'red' }}>{error}</p>
+                <input type="submit" value="Restock" />
+            </form>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
         </div>
     );
 };
