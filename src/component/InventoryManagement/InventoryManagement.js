6266844d302400ee928da705f5fbc2ab86450515
neloy005 <arifistiekofficial@gmail.com>
2022-05-03 03:48:45 +0600

    decrease one item done

--- src/component/InventoryManagement/InventoryManagement.js
+++ src/component/InventoryManagement/InventoryManagement.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const InventoryManagement = () => {
     const { id } = useParams();
 
+    const [fruit, setFruit] = useState({});
+    const [fruitAfterDecrease, setFruitAfterDecrease] = useState(0);
+    const [fruitCount, setFruitCount] = useState(0)
+
+    useEffect(() => {
+        const url = `http://localhost:5000/fruit/${id}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setFruit(data);
+                setFruitCount(data.quantity);
+            });
+    }, [])
+
+
+    let quantity = fruitCount;
+    const handleDecrease = () => {
+        quantity -= 1;
+        setFruitCount(quantity);
+        const decreasedQuantity = { quantity: quantity };
+        const url = `http://localhost:5000/fruit/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(decreasedQuantity)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('success', data);
+            })
+    }
+
+
+
     return (
-        <div>
+        <div className='single-fruit'>
             <h2>Inventory Management: {id}</h2>
+            <p>{fruit.name}</p>
+            <p>{fruitCount}</p>
+            <button onClick={handleDecrease}>Delivered</button>
         </div>
     );
 };
